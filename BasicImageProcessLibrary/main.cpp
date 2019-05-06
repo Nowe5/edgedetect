6dd9cbfdac0d118cc08b59471fbd48df2c3c5276
Ciyotin <ciyotin@gmail.com>
2019-05-07 00:16:04 +0300

    canny done, hough togo

---
+++ BasicImageProcessLibrary/main.cpp
@@ -0,0 +1,12 @@
+#include "BMP.h"
+
+int main()
+{
+    BMP bmp;
+    bmp.readImage("emma.bmp");
+    bmp.saveGrayScale("grayScale");
+   // bmp.cropImage(0,0,374,234);
+    bmp.finalisation();
+
+    return 0;
+}
